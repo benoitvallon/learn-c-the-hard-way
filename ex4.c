748ac135f26f0587816720c7bd59e50fba6fcbb1
Benoit VALLON <benoitvallon@gmail.com>
2014-11-18 23:13:55 +0100

    Adds exercise 4

---
+++ ex4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* Warning: This program is wrong on purpose */
+
+int main()
+{
+    int age = 10;
+    int height;
+
+    printf("I am %d years old.\n");
+    pritnf("I am %d inches tall.\n", height);
+
+    return 0;
+}
